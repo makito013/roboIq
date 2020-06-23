04a62df385d46d593a550b13f7f480460410d86b
makito013 <bandradest@gmail.com>
2020-06-23 16:15:58 -0300

    commit

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from cx_Freeze import setup, Executable 
   
+build_exe_options = {
+                     "packages": ["talib"],
+                     }
+                     
 setup(name = "SuperBot-IQ" , 
       version = "0.1" , 
       description = "" , 
+      options = {"build_exe": build_exe_options},
       executables = [Executable("bot.py", icon="icon.ico")]) 
